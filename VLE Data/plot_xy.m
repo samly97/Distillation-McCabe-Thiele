31d77ae2c4f4345773f06ed9847bbd2b2843b561
samly97 <97.sam.ly@gmail.com>
2020-05-14 17:04:14 -0400

    update xy plots

--- VLE Data/plot_xy.m
+++ VLE Data/plot_xy.m
@@ -1,8 +1,5 @@
-function plot_xy(n)
-    vle = generate_vle_data(n);
-    xEtoh=vle(:,1); yEtoh=vle(:,2); T_eqlm =vle(:,3);
-    x=linspace(0,1,n)'; y=x;
-    save('base_xy_diagram.mat','x','y','xEtoh','yEtoh','T_eqlm')
+function plot_xy()
+    load('base_xy_diagram.mat','x','y','xEtoh','yEtoh')
     
     plot(xEtoh,yEtoh,'b','LineWidth',2)
     hold on
